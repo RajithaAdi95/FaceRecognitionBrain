65bf88714dc0e6a55f581e6e9c81df4183940b2e
RajithaAdi95 <44214404+RajithaAdi95@users.noreply.github.com>
2020-06-03 15:11:49 +0530

    Updates

---
+++ precache-manifest.16d042647b1af2a8a56a4ef6007b73aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583a1a4eccd0b215f1d4b311c733810d",
+    "url": "/FaceRecognitionBrain/index.html"
+  },
+  {
+    "revision": "ba8ec1f8619ad859638e",
+    "url": "/FaceRecognitionBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "37d78aba99fb2b751151",
+    "url": "/FaceRecognitionBrain/static/css/main.7ecd7b79.chunk.css"
+  },
+  {
+    "revision": "ba8ec1f8619ad859638e",
+    "url": "/FaceRecognitionBrain/static/js/2.7699a5ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecognitionBrain/static/js/2.7699a5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d78aba99fb2b751151",
+    "url": "/FaceRecognitionBrain/static/js/main.19a1695e.chunk.js"
+  },
+  {
+    "revision": "589dfa44d84b200fdd56",
+    "url": "/FaceRecognitionBrain/static/js/runtime-main.cb11a66d.js"
+  }
+]);
